cf65bf88101a33d915ed28f7b6e550cd921f9ce1
han <han@hatch.lol>
2025-03-30 16:23:21 -0500

    update site :0

---
+++ navbar.js
@@ -0,0 +1,9 @@
+document.getElementById("navarea").innerHTML = `
+<div id="nav">
+<div id="navimg"><img src="photos/favicon.png" />&nbsp;home</div>
+<div class="navitem">socials</div>
+<div class="navitem">projects</div>
+<div class="navitem">about</div>
+<div class="navitem">more pages</div>
+</div>
+`
